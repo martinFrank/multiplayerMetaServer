8e187dc8fd5459ff849ade8a2b8417e17f1c8e5f
Frank Martin <Martin.Frank@finnova.com>
2021-02-05 07:41:10 +0100

    zwischenstand

--- src/main/java/com/github/martinfrank/multiplayermetaserver/model/Players.java
+++ src/main/java/com/github/martinfrank/multiplayermetaserver/model/Players.java
@@ -5,7 +5,6 @@ import com.github.martinfrank.multiplayerprotocol.meta.PlayerData;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 //Notice: this class is for internal database storage and not meant to be exposed to any clients - use the Protocol for data exchanges!
 public class Players {
@@ -13,15 +12,25 @@ public class Players {
     private List<Player> players = new ArrayList<>();
 
     public Players(){
-        Player player = new Player();
-        player.uuid = "3f5af5c6-537a-4a0a-8458-0e1efb5c9833";
-        player.name = "Mosh";
-        player.email = "some.mail@some.domain";
-        player.pass = "swordFish";
-        player.online = false;
-        player.areaId = "templeTest";
-        player.position = new Position(20,20);
-        players.add(player);
+        Player player1 = new Player();
+        player1.uuid = "mf5af5c6-537a-4a0a-8458-0e1efb5c9833";
+        player1.name = "Bosh";
+        player1.email = "some.more.mail@some.domain";
+        player1.pass = "swordFish";
+        player1.online = false;
+        player1.areaId = "templeTest";
+        player1.position = new Position(10, 10);
+        players.add(player1);
+
+        Player player2 = new Player();
+        player2.uuid = "3f5af5c6-537a-4a0a-8458-0e1efb5c9833";
+        player2.name = "Mosh";
+        player2.email = "some.mail@some.domain";
+        player2.pass = "swordFish";
+        player2.online = false;
+        player2.areaId = "templeTest";
+        player2.position = new Position(20, 10);
+        players.add(player2);
     }
 
     public Player get(String name, String pass) {
